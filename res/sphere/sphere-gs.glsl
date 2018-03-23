813e0526daf11cd40a2c09b60bfddcf5f6c03f51
Stefan Bruckner <stefan.bruckner@uib.no>
2018-03-23 19:00:50 +0100

    better radius bound

--- res/sphere/sphere-gs.glsl
+++ res/sphere/sphere-gs.glsl
@@ -34,11 +34,11 @@ void main()
 	gSphereRadius = sphereRadius;
 
 	vec4 p0 = modelView * vec4(gl_in[0].gl_Position.xyz,1.0);
-	vec4 p1 = modelView * (vec4(gl_in[0].gl_Position.xyz,1.0)+vec4(sphereRadius,sphereRadius,sphereRadius,0.0));	
+	vec4 p1 = modelView * (vec4(gl_in[0].gl_Position.xyz,1.0)+vec4(sphereRadius,0.0,0.0,0.0));	
 	float radius = length(p1.xyz-p0.xyz);
 
-	vec3 up = vec3(0.0, 1.0, 0.0) * radius*0.5*sqrt(3.0);//*sqrt(3.0)*0.5;
-	vec3 right = vec3(1.0, 0.0, 0.0) * radius*0.5*sqrt(3.0);//*sqrt(3.0)*0.5;
+	vec3 up = vec3(0.0, 1.0, 0.0) * radius;//*0.5*sqrt(2.0);//*sqrt(3.0)*0.5;
+	vec3 right = vec3(1.0, 0.0, 0.0) * radius;//*0.5*sqrt(2.0);//*sqrt(3.0)*0.5;
 
 	gFragmentPosition = projection*vec4(p0.xyz - right - up, 1.0);
 	gl_Position = gFragmentPosition;
