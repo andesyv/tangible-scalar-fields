764ad3ce8f8bacce1b785787c09271379d86c91b
Anders Syvertsen <andesyv@gmail.com>
2021-10-09 16:15:19 +0200

    Reading back data from GPU memory

--- src/renderer/CrystalRenderer.cpp
+++ src/renderer/CrystalRenderer.cpp
@@ -3,6 +3,9 @@
 #include "tileRenderer/Tile.h"
 
 #include <array>
+#include <format>
+#include <algorithm>
+#include <utility>
 
 #include <glbinding/gl/enum.h>
 #include <globjects/globjects.h>
@@ -11,10 +14,10 @@
 #include <globjects/State.h>
 #include <globjects/VertexAttributeBinding.h>
 #include <globjects/NamedString.h>
+#include <globjects/Sync.h>
 #include <glm/gtx/string_cast.hpp>
 #include <imgui.h>
 #include <glbinding/gl/bitfield.h>
-#include <format>
 
 using namespace molumes;
 using namespace globjects;
@@ -63,10 +66,16 @@ const auto stateGuard = []() {
 };
 
 constexpr std::size_t MAX_HEXAGON_SIZE = 10000u;
+constexpr auto VERTEXSTORAGEMASK = BufferStorageMask::GL_MAP_PERSISTENT_BIT | BufferStorageMask::GL_MAP_READ_BIT /*| BufferStorageMask::GL_MAP_WRITE_BIT | BufferStorageMask::GL_MAP_COHERENT_BIT*/;
+
+// Waits for as little as it can and checks if the future is ready.
+template <typename T>
+bool isReady(const std::future<T>& f) {
+    return f.wait_for(std::chrono::nanoseconds{0}) == std::future_status::ready;
+}
 
 CrystalRenderer::CrystalRenderer(Viewer *viewer) : Renderer(viewer) {
-    m_vertexBuffer = Buffer::create();
-    m_vertexBuffer->setStorage(6 * 3 * sizeof(vec4) * MAX_HEXAGON_SIZE, nullptr, BufferStorageMask::GL_NONE_BIT);
+    resizeVertexBuffer(MAX_HEXAGON_SIZE);
 
     m_vao = std::make_unique<VertexArray>(); /// Apparently exactly the same as VertexArray::create();
 
@@ -125,11 +134,9 @@ void CrystalRenderer::display() {
     if (lastCount != count) {
         m_hexagonsUpdated = true;
         lastCount = count;
+        drawingCount = count * 6 * 2 * 3;
         // If count has changed, recreate and resize buffer. (expensive operation, so we only do this whenever the count changes)
-        /// Note: glBufferStorage only changes characteristics of how data is stored, so data itself is just as fast when doing glBufferData
-        m_vertexBuffer = Buffer::create();
-        m_vertexBuffer->setStorage(count * 6 * 2 * 3 * static_cast<GLsizeiptr>(sizeof(vec4)), nullptr,
-                                   BufferStorageMask::GL_NONE_BIT);
+        resizeVertexBuffer(count);
     }
 
     const auto num_cols = static_cast<int>(std::ceil(std::sqrt(count)));
@@ -159,6 +166,7 @@ void CrystalRenderer::display() {
     const mat4 model = mScale * mTrans;
     const mat4 modelViewProjectionMatrix = viewer()->projectionTransform() *
                                            viewer()->viewTransform(); // Skipping model matrix as it's supplied another way anyway.
+    std::unique_ptr<Sync> syncObject;
 
     // Calculate triangles:
     if (m_hexagonsUpdated) {
@@ -185,18 +193,32 @@ void CrystalRenderer::display() {
         accumulateMax->unbind(GL_SHADER_STORAGE_BUFFER, 2);
         accumulateTexture->unbindActive(1);
         m_vertexBuffer->unbind(GL_SHADER_STORAGE_BUFFER, 0);
+
+        /** Returning data from GPU:
+         * 1. Memory barrier
+         * 2. Create and initiate fence-sync object
+         * 3. Initiate rest of drawing commands for frame
+         * 4. Wait for sync at end of frame (remember to glFlush)
+         * 5. Map buffer, copy data.
+         * 6. Create thread and pass data to thread
+         * 6. Unmap buffer
+         */
+
+        // We are going to use the buffer to read from, but also to
+        glMemoryBarrier(GL_CLIENT_MAPPED_BUFFER_BARRIER_BIT | GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
+        syncObject = std::move(Sync::fence(GL_SYNC_GPU_COMMANDS_COMPLETE));
+        m_hexagonsUpdated = false;
+
+//        std::promise<std::optional<std::vector<glm::vec4>>> tempPromise;
+//        m_workerResult = std::move(tempPromise.get_future());
+//        tempPromise.set_value(geometryPostProcessing(std::move(Sync::fence(GL_SYNC_GPU_COMMANDS_COMPLETE)), count, std::weak_ptr{m_vertexBuffer}));
     }
 
     // Render triangles:
     {
         const auto &shader = shaderProgram("standard");
-        if (!shader)
+        if (!shader || drawingCount < 1)
             return;
-
-        if (m_hexagonsUpdated) {
-            glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
-            m_hexagonsUpdated = false;
-        }
 
         shader->use();
         shader->setUniform("MVP", modelViewProjectionMatrix);
@@ -210,10 +232,32 @@ void CrystalRenderer::display() {
         binding->setFormat(4, GL_FLOAT);
         m_vao->enable(0);
 
-        m_vao->drawArrays(GL_TRIANGLES, 0, count * 6 * 2 * 3);
+        m_vao->drawArrays(GL_TRIANGLES, 0, drawingCount);
     }
 
     globjects::Program::release();
+
+    // Background memory management:
+    if (syncObject) {
+        const auto syncResult = syncObject->clientWait(GL_SYNC_FLUSH_COMMANDS_BIT, 10000);
+        if (syncResult != GL_WAIT_FAILED && syncResult != GL_TIMEOUT_EXPIRED) {
+            const auto vCount = count * 6 * 2 * 3;
+            const auto memPtr = reinterpret_cast<vec4*>(m_vertexBuffer->map(GL_READ_ONLY));
+            m_workerResult = std::move(std::async(std::launch::async, geometryPostProcessing, std::vector<vec4>{memPtr, memPtr + vCount}, m_vertexBuffer));
+            assert(m_vertexBuffer->unmap());
+        }
+    }
+
+    // If worker is done with doing its things, replace the buffer with new values:
+    if (m_workerResult.valid() && isReady(m_workerResult)) {
+        auto result = m_workerResult.get();
+        if (result) {
+            // Orphan that buffer!
+            m_vertexBuffer = Buffer::create();
+            m_vertexBuffer->setStorage( static_cast<GLsizeiptr>(result->size() * sizeof(vec4)), nullptr, VERTEXSTORAGEMASK);
+            drawingCount = static_cast<int>(result->size());
+        }
+    }
 }
 
 uint CrystalRenderer::calculateTriangleCount(int hexCount) {
@@ -239,4 +283,39 @@ glm::uint CrystalRenderer::calculateEdgeCount(int hexCount) {
     }
 
     return sum;
+}
+
+void CrystalRenderer::resizeVertexBuffer(int hexCount) {
+    const auto vCount = hexCount * 6 * 2 * 3;
+    // Make sure the buffer can be read from
+
+    m_vertexBuffer = Buffer::create();
+    /// Note: glBufferStorage only changes characteristics of how data is stored, so data itself is just as fast when doing glBufferData
+    m_vertexBuffer->setStorage(vCount * static_cast<GLsizeiptr>(sizeof(vec4)), nullptr, VERTEXSTORAGEMASK);
+
+    m_vertexBuffer->bind(GL_SHADER_STORAGE_BUFFER);
+//    m_vertexBuffer->map(GL_READ_WRITE)
+//    m_vertexDataPtr = PersistentStoragePtr<glm::vec4>{m_vertexBuffer.get()};
+}
+
+std::optional<std::vector<vec4>> CrystalRenderer::geometryPostProcessing(const std::vector<vec4>& vertices, const std::weak_ptr<Buffer>& bufferPtr) {
+    // If we at this point don't have a buffer, it means it got recreated somewhere in the meantime.
+    // In which case we don't need this thread anymore.
+    if (bufferPtr.expired())
+        return {};
+
+    // Filter out empty vertices
+    std::vector<vec4> data;
+    data.reserve(vertices.size());
+    std::copy_if(vertices.begin(), vertices.end(), std::back_inserter(data), [](const auto& v){ return 0.1f < v.w; });
+
+    // Do stuff:..
+    if (bufferPtr.expired())
+        return {};
+
+    return data;
+//    // After everything is done, create new buffer with new vertex data and return it
+//    std::shared_ptr<Buffer> newBuffer = Buffer::create();
+//    newBuffer->setStorage(vCount * static_cast<GLsizeiptr>(sizeof(vec4)), data.data(), VERTEXSTORAGEMASK);
+//    return std::make_pair(std::move(newBuffer), vCount);
 }
--- src/renderer/CrystalRenderer.h
+++ src/renderer/CrystalRenderer.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #include <memory>
+#include <future>
+#include <optional>
+
 #include "Renderer.h"
 
 namespace globjects {
     class VertexArray;
 
     class Buffer;
+    class Sync;
 }
 
 namespace molumes {
@@ -22,14 +26,18 @@ namespace molumes {
 
     private:
         std::unique_ptr<globjects::VertexArray> m_vao;
-        std::unique_ptr<globjects::Buffer> m_vertexBuffer;
+        std::shared_ptr<globjects::Buffer> m_vertexBuffer; // Using shared_ptr to check if shared resource is same
+        std::future<std::optional<std::vector<glm::vec4>>> m_workerResult;
 
         bool m_wireframe = false;
         float m_tileScale = 1.0f;
         float m_tileHeight = 1.0f;
         bool m_hexagonsUpdated = true;
+        gl::GLsizei drawingCount = 0;
 
         static glm::uint calculateTriangleCount(int hexCount);
         static glm::uint calculateEdgeCount(int hexCount);
+        void resizeVertexBuffer(int hexCount);
+        static std::optional<std::vector<glm::vec4>> geometryPostProcessing(const std::vector<glm::vec4>& vertices, const std::weak_ptr<globjects::Buffer>& bufferPtr);
     };
 }
