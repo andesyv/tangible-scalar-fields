8f7f5ad1f0e492c4591697b72e9175c5f683a7ac
Anders Syvertsen <andesyv@gmail.com>
2021-10-15 20:58:03 +0200

    Fixed graham scan

--- src/renderer/CrystalRenderer.cpp
+++ src/renderer/CrystalRenderer.cpp
@@ -70,16 +70,63 @@ const auto stateGuard = []() {
 };
 
 constexpr std::size_t MAX_HEXAGON_SIZE = 10000u;
-constexpr auto VERTEXSTORAGEMASK = BufferStorageMask::GL_MAP_PERSISTENT_BIT | BufferStorageMask::GL_MAP_READ_BIT /*| BufferStorageMask::GL_MAP_WRITE_BIT | BufferStorageMask::GL_MAP_COHERENT_BIT*/;
+constexpr auto VERTEXSTORAGEMASK = BufferStorageMask::GL_MAP_PERSISTENT_BIT |
+                                   BufferStorageMask::GL_MAP_READ_BIT /*| BufferStorageMask::GL_MAP_WRITE_BIT | BufferStorageMask::GL_MAP_COHERENT_BIT*/;
 
 // Waits for as little as it can and checks if the future is ready.
-template <typename T>
-bool isReady(const std::future<T>& f) {
+template<typename T>
+bool isReady(const std::future<T> &f) {
     return f.wait_for(std::chrono::nanoseconds{0}) == std::future_status::ready;
 }
 
-template <std::forward_iterator It, typename F>
-auto filter(It begin, It end, F&& predicate) {
+template<typename Iter>
+class cyclic_iterator {
+public:
+    using difference_type = typename Iter::difference_type;
+
+private:
+    const Iter _beg;
+    const Iter _end;
+    difference_type _rangeSize;
+
+public:
+    difference_type i = 0;
+
+    cyclic_iterator(Iter range_begin, Iter range_end, const difference_type &offset)
+            : _beg{std::move(range_begin)}, _end{std::move(range_end)}, i{offset},
+              _rangeSize{std::distance(_beg, _end)} {}
+
+    cyclic_iterator(const cyclic_iterator &) = default;
+
+    auto operator++() {
+        ++i;
+        return *this;
+    }
+
+    auto operator--() {
+        --i;
+        return *this;
+    }
+
+    auto get_iterator() {
+        return (i < 0 ? _end : _beg) + static_cast<difference_type>(i % _rangeSize);
+    }
+
+    auto operator*() {
+        return *get_iterator();
+    }
+
+    auto operator+(const difference_type &diff) {
+        return cyclic_iterator{_beg, _end, i + diff};
+    }
+
+    auto operator-(const difference_type &diff) {
+        return *this + (-diff);
+    }
+};
+
+template<std::forward_iterator It, typename F>
+auto filter(It begin, It end, F &&predicate) {
     using T = decltype(*(It{}));
     std::vector<std::remove_cvref_t<T>> out;
     out.reserve(std::distance(begin, end));
@@ -87,8 +134,8 @@ auto filter(It begin, It end, F&& predicate) {
     return out;
 }
 
-template <std::forward_iterator It, typename F>
-auto map(It begin, It end, F&& predicate) {
+template<std::forward_iterator It, typename F>
+auto map(It begin, It end, F &&predicate) {
     using T = typename It::value_type; // Only works on iterators with value_type member
     using K = decltype(predicate(T{}));
     std::vector<std::remove_cvref_t<K>> out;
@@ -97,19 +144,19 @@ auto map(It begin, It end, F&& predicate) {
     return out;
 }
 
-auto scalarCross2D(const dvec2& a, const dvec2& b) {
+auto scalarCross2D(const dvec2 &a, const dvec2 &b) {
     const auto c = cross(dvec3{a, 0.0}, dvec3{b, 0.0});
     return c.z;
 }
 
-bool ccw(const dvec2& a, const dvec2& b, double epsilon = 0.001f) {
-    return -epsilon < scalarCross2D(a,b);
+bool ccw(const dvec2 &a, const dvec2 &b, double epsilon = 0.001f) {
+    return epsilon <= scalarCross2D(a, b);
 }
 
-double dotBetween(const dvec2& a, const dvec2& b) {
-    // 0.25 * [-1,1] + 0.25 -> [0,0.5]
-    // -0.25 * [1,-1] + 0.75 -> [0.5,1]
-    return ccw(a,b) ? (0.25 * dot(a,b) + 0.25) : (-0.25 * dot(a,b) + 0.75);
+double dotBetween(const dvec2 &a, const dvec2 &b) {
+    // -0.25 * [1,-1] + 0.25 -> [0,0.5]
+    // 0.25 * [-1,1] + 0.75 -> [0.5,1]
+    return ccw(a, b) ? (-0.25 * dot(a, b) + 0.25) : (0.25 * dot(a, b) + 0.75);
 }
 
 //bool insideHull(const vec3& p, const std::vector)
@@ -142,7 +189,8 @@ geometryPostProcessing(const std::vector<vec4> &vertices, const std::weak_ptr<Bu
         if (-tileHeight < v.z) {
             nonEmptyValues.emplace_back(w, i);
             // Also find smallest (for graham scan later)
-            if (data[i].y < data[first].y && data[i].x + EPS < data[first].x) // y' < y && x' <= x
+            const auto& oldSmallest = std::get<0>(nonEmptyValues[first]);
+            if (w.y < oldSmallest.y || w.y < oldSmallest.y + EPS && w.x < oldSmallest.x) // y' < y && x' <= x
                 first = static_cast<uint>(nonEmptyValues.size() - 1);
         }
     }
@@ -157,7 +205,7 @@ geometryPostProcessing(const std::vector<vec4> &vertices, const std::weak_ptr<Bu
     // NB: Only works on 0 < size()
     std::vector<std::tuple<dvec2, uint, double>> nonEmptyValuesPolarAngled;
     nonEmptyValuesPolarAngled.reserve(nonEmptyValues.size());
-    for (const auto& [p, i] : nonEmptyValues) {
+    for (const auto&[p, i]: nonEmptyValues) {
         if (nonEmptyValues[0].second == i) {
             nonEmptyValuesPolarAngled.emplace_back(p, i, 0.f);
         } else {
@@ -187,33 +235,20 @@ geometryPostProcessing(const std::vector<vec4> &vertices, const std::weak_ptr<Bu
                 continue;
         }
 
-        bool invalidPoint = false;
         // While we have enough points in our stack...
         while (2 <= convexHullStack.size()) {
             const auto last = std::get<0>(convexHullStack[0]);
             const auto second_last = std::get<0>(convexHullStack[1]);
             auto a = last - second_last;
-            const auto la = length(a);
             auto b = p - last;
-            const auto lb = length(b);
-
-//                if (la < EPS || lb < EPS) {
-//                    invalidPoint = true;
-//                    break;
-//                }
-            // Don't need to normalize, as I only use the cross product to check sign
-//                a *= 1.0 / la;
-//                b *= 1.0 / lb;
 
             // If the new point forms a clockwise turn with the last points in the stack,
             // pop the last point from the stack
-            if (!ccw(a, b, EPS))
+            if (ccw(a, b, EPS))
                 break;
             else
                 convexHullStack.pop_front();
         }
-        if (invalidPoint)
-            continue;
 
         // Since points are sorted on polar coordinates and empty increments are skipped,
         // no point that arrives here should give an empty increment
@@ -221,31 +256,54 @@ geometryPostProcessing(const std::vector<vec4> &vertices, const std::weak_ptr<Bu
     }
 
     // My Graham scan implementation isn't perfect, so traverse hull once more and fix concave edges
-    auto end = convexHullStack.rend();
     // TODO: Only need to fix start / end point
-//    for (auto last{convexHullStack.rbegin()}, it{convexHullStack.rbegin()+1}; it != end; last = it, ++it) {
+    // Last point could, but doesn't have to, be part of the convex hull. So we start on the one before.
+
+//    const std::size_t startOffset{convexHullStack.size() / 2};
+//    auto beg{convexHullStack.rbegin()}, end{convexHullStack.rend()};
+//    std::vector<decltype(convexHullStack.rbegin())> copyRanges;
+//    copyRanges.push_back(convexHullStack.rbegin());
+//    cyclic_iterator it{convexHullStack.rbegin(), convexHullStack.rend(),
+//                       static_cast<decltype(beg)::difference_type>(startOffset)};
+//    for (std::size_t i{0}; i < convexHullStack.size(); ++i, ++it) {
 //        auto next = it + 1;
-//        if (next == end) break; // Early break
+//        auto last = it - 1;
 //
 //        const auto curr_p = std::get<0>(*it);
 //        const auto last_p = std::get<0>(*last);
 //        const auto a = normalize(curr_p - last_p);
 //
-//        for (; next != end; ++next) {
+//        for (uint j{0}; j < convexHullStack.size() - 1; ++j, ++next) {
 //            const auto next_p = std::get<0>(*next);
 //            /// Should be safe to normalize as we've discarded zero-length vectors previously
 //            const auto b = normalize(next_p - curr_p);
 //
-//            if (ccw(a,b))
+//            if (ccw(a, b))
 //                break;
 //        }
 //
+//
+//
 //        // If we skipped some points, update range by "deleting" them.
-//        if (1 < std::distance(it, next)) {
-//            // Delete them by moving the range after the skip to the start of the skip. And update end
-//            end = std::move(next, end, it+1);
+//        auto offsetEnd = std::distance(beg, it.get_iterator());
+//        auto offsetBeg = std::distance(beg, next.get_iterator());
+//        if (1 < std::abs(offsetEnd - offsetBeg)) {
+//            auto e{it.get_iterator()}, b{next.get_iterator()};
+//            if (offsetEnd < offsetBeg) {
+//                copyRanges.push_back(beg + offsetEnd);
+//                copyRanges.push_back(beg + offsetBeg);
+//            } else {
+//                copyRanges.push_back(beg + offsetBeg);
+//                copyRanges.push_back(beg + offsetEnd);
+//            }
 //        }
 //    }
+//
+//    copyRanges.push_back(end);
+//    std::vector<std::tuple<dvec2, uint, double>> hullList;
+//    hullList.reserve(convexHullStack.size());
+//    for (int i{0}; i < copyRanges.size(); i += 2)
+//        hullList.insert(hullList.end(), copyRanges.at(i), copyRanges.at(i + 1));
 
     // Optimization idea: Since the hull is sorted by polar angles, we knot roughly where to find a hull edge based on its angle.
     // So we can use a divide and conquer search instead of searching through all of them. (ex. recursively)
@@ -254,12 +312,12 @@ geometryPostProcessing(const std::vector<vec4> &vertices, const std::weak_ptr<Bu
     // Hull is in clockwise order, meaning any points inside the hull should make a clockwise turn in respect to the hull
     // Any point who doesn't make a clockwise turn is outside the hull
 
-    const auto hullList = map(convexHullStack.rbegin(), end, [tileHeight](const auto& t){
+    const auto hullListConverted = map(convexHullStack.rbegin(), convexHullStack.rend(), [tileHeight](const auto &t) {
         const auto p = std::get<0>(t);
         return glm::vec4{p.x, p.y, -tileHeight, 1.f};
     });
 
-    return bufferPtr.expired() ? std::nullopt : std::make_optional(std::make_pair(data, hullList));
+    return bufferPtr.expired() ? std::nullopt : std::make_optional(std::make_pair(data, hullListConverted));
 }
 
 CrystalRenderer::CrystalRenderer(Viewer *viewer) : Renderer(viewer) {
@@ -297,6 +355,7 @@ void CrystalRenderer::setEnabled(bool enabled) {
 
 void CrystalRenderer::display() {
     const auto state = stateGuard();
+    static float hullDegree = 1.f;
 
     if (ImGui::BeginMenu("Crystal")) {
         ImGui::Checkbox("Wireframe", &m_wireframe);
@@ -304,6 +363,7 @@ void CrystalRenderer::display() {
             m_hexagonsUpdated = true;
         if (ImGui::SliderFloat("Height", &m_tileHeight, 0.01f, 1.f))
             m_hexagonsUpdated = true;
+        ImGui::SliderFloat("Hull degree", &hullDegree, 0.01f, 1.f);
 
         ImGui::EndMenu();
     }
@@ -446,7 +506,7 @@ void CrystalRenderer::display() {
             binding->setFormat(4, GL_FLOAT);
             m_vao->enable(0);
 
-            m_vao->drawArrays(GL_LINE_STRIP, 0, m_hullSize);
+            m_vao->drawArrays(GL_LINE_STRIP, 0, static_cast<int>(m_hullSize * hullDegree));
         }
     }
 
@@ -457,12 +517,17 @@ void CrystalRenderer::display() {
         const auto syncResult = syncObject->clientWait(GL_SYNC_FLUSH_COMMANDS_BIT, 100000000);
         if (syncResult != GL_WAIT_FAILED && syncResult != GL_TIMEOUT_EXPIRED) {
             const auto vCount = count * 6 * 2 * 3;
-            const auto memPtr = reinterpret_cast<vec4*>(m_computeBuffer->mapRange(0, vCount * static_cast<GLsizeiptr>(sizeof(vec4)), GL_MAP_READ_BIT));
+            const auto memPtr = reinterpret_cast<vec4 *>(m_computeBuffer->mapRange(0, vCount *
+                                                                                      static_cast<GLsizeiptr>(sizeof(vec4)),
+                                                                                   GL_MAP_READ_BIT));
             if (memPtr != nullptr)
-                m_workerResult = std::move(std::async(std::launch::async, geometryPostProcessing, std::vector<vec4>{memPtr + 0, memPtr + vCount}, m_computeBuffer, m_tileHeight));
+                m_workerResult = std::move(std::async(std::launch::async, geometryPostProcessing,
+                                                      std::vector<vec4>{memPtr + 0, memPtr + vCount}, m_computeBuffer,
+                                                      m_tileHeight));
             assert(m_computeBuffer->unmap());
         } else {
-            std::cout << "Error: Sync Object was " << (syncResult == GL_WAIT_FAILED ? "GL_WAIT_FAILED" :  "GL_TIMEOUT_EXPIRED") << std::endl;
+            std::cout << "Error: Sync Object was "
+                      << (syncResult == GL_WAIT_FAILED ? "GL_WAIT_FAILED" : "GL_TIMEOUT_EXPIRED") << std::endl;
         }
     }
 
@@ -470,14 +535,16 @@ void CrystalRenderer::display() {
     if (m_workerResult.valid() && isReady(m_workerResult)) {
         auto result = m_workerResult.get();
         if (result) {
-            const auto& [vertices, hull] = *result;
+            const auto&[vertices, hull] = *result;
             // Create new buffer subset (unlinking m_computeBuffer)
             m_vertexBuffer = Buffer::create();
-            m_vertexBuffer->setStorage( static_cast<GLsizeiptr>(vertices.size() * sizeof(vec4)), vertices.data(), VERTEXSTORAGEMASK);
+            m_vertexBuffer->setStorage(static_cast<GLsizeiptr>(vertices.size() * sizeof(vec4)), vertices.data(),
+                                       VERTEXSTORAGEMASK);
             m_drawingCount = static_cast<int>(vertices.size());
 
             m_hullBuffer = Buffer::create();
-            m_hullBuffer->setStorage(static_cast<GLsizeiptr>(hull.size() * sizeof(vec4)), hull.data(), VERTEXSTORAGEMASK);
+            m_hullBuffer->setStorage(static_cast<GLsizeiptr>(hull.size() * sizeof(vec4)), hull.data(),
+                                     VERTEXSTORAGEMASK);
             m_hullSize = static_cast<int>(hull.size());
         }
     }
@@ -515,7 +582,8 @@ void CrystalRenderer::resizeVertexBuffer(int hexCount) {
     m_computeBuffer = Buffer::create();
     /// Note: glBufferStorage only changes characteristics of how data is stored, so data itself is just as fast when doing glBufferData
     m_computeBuffer->setStorage(vCount * static_cast<GLsizeiptr>(sizeof(vec4)), nullptr, VERTEXSTORAGEMASK);
-    assert(m_computeBuffer->getParameter(GL_BUFFER_SIZE) == vCount * static_cast<GLsizeiptr>(sizeof(vec4))); // Check that requested size == actual size
+    assert(m_computeBuffer->getParameter(GL_BUFFER_SIZE) ==
+           vCount * static_cast<GLsizeiptr>(sizeof(vec4))); // Check that requested size == actual size
 
     m_computeBuffer->bind(GL_SHADER_STORAGE_BUFFER);
 //    m_vertexBuffer->map(GL_READ_WRITE)
