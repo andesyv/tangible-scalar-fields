4ab679aac09d533b728a44fd159b7583b06130b6
Anders Syvertsen <andesyv@gmail.com>
2021-10-22 00:03:45 +0200

    Attempted some hexagonal convex hull

--- src/GeometryUtils.cpp
+++ src/GeometryUtils.cpp
@@ -8,6 +8,7 @@
 #include <glm/glm.hpp>
 
 #include <globjects/Buffer.h>
+#include <glm/gtc/constants.hpp>
 
 using namespace glm;
 
@@ -49,8 +50,169 @@ namespace molumes {
         return std::find(beg, end, val) != end;
     }
 
+    template<typename T, int N>
+    std::weak_ordering
+    compareVec(const vec<N, T> &as, const vec<N, T> &bs, unsigned int i = 0, float epsilon = 0.0001f) {
+        return N <= i ? std::weak_ordering::equivalent : (as[i] < bs[i] + epsilon && bs[i] < as[i] + epsilon) ?
+                                                         compareVec(as, bs, i + 1, epsilon) :
+                                                         ((as[i] < bs[i]) ? std::weak_ordering::less
+                                                                          : std::weak_ordering::greater);
+    }
+
+    struct Vec4Comparitor {
+        auto operator()(const vec4 &lhs, const vec4 &rhs) const {
+            return compareVec(lhs, rhs) == std::weak_ordering::less;
+        }
+    };
+
+    std::pair<std::vector<vec4>, std::vector<unsigned int>>
+    getVertexIndexPairs(const std::vector<vec4> &vertices) {
+        std::vector<vec4> uniqueVertices;
+        uniqueVertices.reserve(vertices.size());
+        std::vector<unsigned int> indices;
+        indices.reserve(vertices.size());
+
+        std::map<vec4, unsigned int, Vec4Comparitor> lookupMap;
+
+        for (const auto &v: vertices) {
+            const auto pos = lookupMap.find(v);
+            if (pos != lookupMap.end()) {
+                indices.push_back(pos->second);
+            } else {
+                uniqueVertices.push_back(v);
+                const auto i = static_cast<unsigned int>(lookupMap.size());
+                indices.push_back(i);
+                lookupMap.emplace(v, i);
+            }
+        }
+
+        uniqueVertices.shrink_to_fit();
+        indices.shrink_to_fit();
+
+        return std::make_pair(uniqueVertices, indices);
+    }
+
+    auto sortedEdge(auto a, auto b) {
+        return a < b ? std::make_pair(a, b) : std::make_pair(b, a);
+    }
+
+    std::set<std::pair<unsigned int, unsigned int>> getEdges(const std::vector<unsigned int> &indices) {
+        std::set<std::pair<unsigned int, unsigned int>> edges;
+
+        for (uint i{0}; i + 2 < indices.size(); i += 3) {
+            for (const auto &edge: {
+                    sortedEdge(indices.at(i), indices.at(i + 1)),
+                    sortedEdge(indices.at(i + 1), indices.at(i + 2)),
+                    sortedEdge(indices.at(i + 2), indices.at(i))
+            })
+                edges.insert(edge);
+        }
+
+        return edges;
+    }
+
+    // Graham scan implementation (first point should be start point / bounds max)
+    std::vector<std::pair<glm::vec2, unsigned int>>
+    createConvexHull(const std::vector<std::pair<glm::dvec2, unsigned int>> &points, const std::set<std::pair<unsigned int, unsigned int>>& edges, glm::dvec2 boundingCenter = {},
+                     const std::weak_ptr<bool> &controlFlag = {}) {
+        // Set comparison direction, the start of the polar angle circle, to be the first point
+        const auto compareDir = normalize(points[0].first - boundingCenter);
+
+        if (controlFlag.expired() || points.empty() || edges.empty()) return {};
+
+        std::vector<std::tuple<dvec2, double, unsigned int>> nonEmptyValuesPolarAngled;
+        nonEmptyValuesPolarAngled.reserve(points.size());
+        nonEmptyValuesPolarAngled.emplace_back(points.at(0).first, 0.0, points.at(0).second);
+        for (auto it{points.begin() + 1}; it != points.end(); ++it) {
+            const auto &[p, i] = *it;
+            const auto dir = p - boundingCenter;
+            const auto dir_len = length(dir);
+            if (EPS < dir_len) {
+                const auto pa = dotBetween(compareDir, dir * (1. / dir_len));
+                nonEmptyValuesPolarAngled.emplace_back(p, pa, i);
+            }
+        }
+
+        if (controlFlag.expired()) return {};
+
+        // Sort points on polar angle
+        std::sort(nonEmptyValuesPolarAngled.begin(), nonEmptyValuesPolarAngled.end(), [](const auto &a, const auto &b) {
+            return std::get<1>(a) < std::get<1>(b);
+        });
+
+        if (controlFlag.expired()) return {};
+
+        // Graham scan implementation:
+        std::deque<std::pair<dvec2, unsigned int>> convexHullStack{};
+        for (const auto&[p, polar_angle, i]: nonEmptyValuesPolarAngled) {
+            // Skip empty/near-empty increments:
+            if (!convexHullStack.empty()) {
+                const auto last = convexHullStack[0].first;
+                const auto b = p - last;
+
+                if (length(b) < EPS)
+                    continue;
+            }
+
+            while (2 <= convexHullStack.size()) {
+                const auto last = convexHullStack[0].first;
+                const auto second_last = convexHullStack[1].first;
+                auto a = last - second_last;
+                auto b = p - last;
+
+                // If the new point forms a clockwise turn with the last points in the stack,
+                // pop the last point from the stack
+                if (!ccw(a, b, EPS) && edges.contains(sortedEdge(i, convexHullStack[1].second)))
+                // Only pop last edge if new edge is a "valid" edge
+                    convexHullStack.pop_front();
+                else
+                    break;
+            }
+
+            // Since points are sorted on polar coordinates and empty increments are skipped,
+            // no point that arrives here should give an empty increment
+
+            // Only add points that has a connected edge (or if there are no edges yet)
+//            if (convexHullStack.empty() || edges.contains(sortedEdge(i, convexHullStack[0].second)))
+                convexHullStack.emplace_front(p, i);
+
+            if (controlFlag.expired()) return {};
+        }
+
+        return map(convexHullStack.rbegin(), convexHullStack.rend() - 1,
+                   [](const auto &h) {
+                       const auto&[pos, i] = h;
+                       return std::make_pair(glm::vec2{pos.x, pos.y}, i);
+                   });
+    }
+
+    std::vector<std::pair<glm::vec2, unsigned int>>
+    crawlAlongEdges(const std::vector<std::pair<glm::dvec2, unsigned int>> &points, const std::set<std::pair<unsigned int, unsigned int>>& edges, glm::dvec2 boundingCenter = {},
+                     const std::weak_ptr<bool> &controlFlag = {}) {
+        std::vector<std::pair<glm::vec2, unsigned int>> result;
+        result.reserve(points.size());
+        result.emplace_back(points.front());
+        result.emplace_back(*std::find_if(edges.begin(), edges.end(), [&points](const auto& e){ return e.first == points.front().second; }));
+
+        for (unsigned int i{0}; i < 10; ++i) {
+            unsigned int k = (result.end() - 2)->second;
+            unsigned int j = result.back().second;
+            const auto a = 
+            auto upper = edges.upper_bound(sortedEdge(j+1u, 0));
+            upper = upper == edges.end() ? edges.end() : upper - 1;
+            auto lower = i == 0 ? edges.begin() : edges.lower_bound(sortedEdge(j-1u, 0u-1u));
+
+            auto nextEdge = lower;
+            auto smallestAngle = glm::two_pi<float>();
+            for (auto it{lower+1}; it != upper; ++it) {
+                float angle = dotBetween()
+                if ()
+            }
+        }
+    }
+
     std::optional<std::pair<std::vector<vec4>, std::vector<vec4>>>
-    geometryPostProcessing(const std::vector<vec4> &vertices, const std::weak_ptr<bool>& controlFlag,
+    geometryPostProcessing(const std::vector<vec4> &vertices, const std::weak_ptr<bool> &controlFlag,
                            float tileHeight) {
         // If we at this point don't have a buffer, it means it got recreated somewhere in the meantime.
         // In which case we don't need this thread anymore.
@@ -58,7 +220,7 @@ namespace molumes {
             return std::nullopt;
 
         // Filter out empty vertices
-        const auto filterEmpties = [](const auto& points) {
+        const auto filterEmpties = [](const auto &points) {
             std::vector<vec4> data;
             data.reserve(points.size());
             for (auto it{points.begin()};
@@ -72,15 +234,15 @@ namespace molumes {
 
         if (controlFlag.expired()) return std::nullopt;
 
-        auto [uniqueVs, indices] = getVertexIndexPairs(filterEmpties(vertices));
+        auto[uniqueVs, indices] = getVertexIndexPairs(filterEmpties(vertices));
 
         // Find the points which has a non-zero value of z (z height is hex-value, meaning empty ones are empty hexes)
         std::vector<std::pair<dvec2, uint>> nonEmptyValues;
         uint first = 0;
         dvec2 mi{std::numeric_limits<float>::max()}, ma{std::numeric_limits<float>::min()};
         nonEmptyValues.reserve(indices.size());
-        for (uint i = 0; i < indices.size(); ++i) {
-            const auto &v = uniqueVs.at(indices.at(i));
+        for (unsigned int i: indices) {
+            const auto &v = uniqueVs.at(i);
             const auto w = dvec2{v.x, v.y};
             mi = min(w, mi);
             ma = max(w, ma);
@@ -101,159 +263,38 @@ namespace molumes {
         // Make sure smallest is first
         std::swap(nonEmptyValues[0], nonEmptyValues[first]);
 
+        const auto edges = getEdges(indices);
+
         // Create convex hull
-        const auto convexHull = createConvexHull(map(nonEmptyValues.begin(), nonEmptyValues.end(), [](const auto &t) {
-            return std::get<0>(t);
-        }), boundingCenter, controlFlag);
+        const auto convexHull = createConvexHull(nonEmptyValues, edges, boundingCenter, controlFlag);
 
         if (controlFlag.expired()) return std::nullopt;
+
+        const auto fst = [](const auto &h) { return h.first; };
 
         // Filter out all points outside of hull from empty values
         // Non-empty values will always be inside the hull
         for (int i{static_cast<int>(indices.size()) - 3}; -1 < i; i -= 3) {
             const auto it = indices.begin() + i;
-            if (std::all_of(it, it + 3, [&convexHull, &uniqueVs = uniqueVs](const auto &j) { // &uniqueVs = uniqueVs is a temporary fix until C++23 fixes structured bindings
+            if (std::all_of(it, it + 3, [hull = map(convexHull.begin(), convexHull.end(), fst), &uniqueVs = uniqueVs](
+                    const auto &j) { // &uniqueVs = uniqueVs is a temporary fix until C++23 fixes structured bindings
                 const auto p = vec2{uniqueVs.at(j)};
-                return !insideHull(p, convexHull);
+                return !insideHull(p, hull);
             }))
                 indices.erase(it, it + 3);
         }
 
-        uniqueVs = map(indices.begin(), indices.end(), [&uniqueVs = uniqueVs](auto i){ return uniqueVs.at(i); });
 
         if (controlFlag.expired()) return std::nullopt;
 
-        const auto hullListConverted = map(convexHull.begin(), convexHull.end(), [tileHeight](const auto &v) {
-            return vec4{v.x, v.y, -tileHeight, 1.f};
-        });
+        const auto hullListConverted = map(convexHull.begin(), convexHull.end(),
+                                           [&uniqueVs = uniqueVs, tileHeight](const auto &v) {
+                                               const auto &pos = uniqueVs.at(v.second);
+                                               return vec4{pos.x, pos.y, -tileHeight, 1.f};
+                                           });
 
+        uniqueVs = map(indices.begin(), indices.end(), [&uniqueVs = uniqueVs](auto i) { return uniqueVs.at(i); });
 
         return controlFlag.expired() ? std::nullopt : std::make_optional(std::make_pair(uniqueVs, hullListConverted));
-    }
-
-    std::vector<glm::vec2> createConvexHull(const std::vector<glm::dvec2> &points, glm::dvec2 boundingCenter,
-                                            const std::weak_ptr<bool>& controlFlag) {
-        // Set comparison direction, the start of the polar angle circle, to be the first point
-        const auto compareDir = normalize(points[0] - boundingCenter);
-
-        if (controlFlag.expired()) return {};
-
-        std::vector<std::pair<dvec2, double>> nonEmptyValuesPolarAngled;
-        nonEmptyValuesPolarAngled.reserve(points.size());
-        nonEmptyValuesPolarAngled.emplace_back(points.at(0), 0.0);
-        for (auto it{points.begin() + 1}; it != points.end(); ++it) {
-            const auto &p = *it;
-            const auto dir = p - boundingCenter;
-            const auto dir_len = length(dir);
-            if (EPS < dir_len) {
-                const auto pa = dotBetween(compareDir, dir * (1. / dir_len));
-                nonEmptyValuesPolarAngled.emplace_back(p, pa);
-            }
-        }
-
-        if (controlFlag.expired()) return {};
-
-        // Sort points on polar angle
-        std::sort(nonEmptyValuesPolarAngled.begin(), nonEmptyValuesPolarAngled.end(), [](const auto &a, const auto &b) {
-            return std::get<1>(a) < std::get<1>(b);
-        });
-
-        if (controlFlag.expired()) return {};
-
-        // Graham scan implementation:
-        std::deque<dvec2> convexHullStack{};
-        for (const auto&[p, polar_angle]: nonEmptyValuesPolarAngled) {
-            // Skip empty/near-empty increments:
-            if (!convexHullStack.empty()) {
-                const auto last = convexHullStack[0];
-                const auto b = p - last;
-
-                if (length(b) < EPS)
-                    continue;
-            }
-
-            while (2 <= convexHullStack.size()) {
-                const auto last = convexHullStack[0];
-                const auto second_last = convexHullStack[1];
-                auto a = last - second_last;
-                auto b = p - last;
-
-                // If the new point forms a clockwise turn with the last points in the stack,
-                // pop the last point from the stack
-                if (ccw(a, b, EPS))
-                    break;
-                else
-                    convexHullStack.pop_front();
-            }
-
-            // Since points are sorted on polar coordinates and empty increments are skipped,
-            // no point that arrives here should give an empty increment
-            convexHullStack.push_front(p);
-
-            if (controlFlag.expired()) return {};
-        }
-
-        return map(convexHullStack.rbegin(), convexHullStack.rend() - 1,
-                   [](const auto &p) {
-                       return glm::vec2{p.x, p.y};
-                   });
-    }
-
-    template <typename T, int N>
-    std::weak_ordering compareVec(const vec<N, T>& as, const vec<N, T>& bs, unsigned int i = 0, float epsilon = 0.0001f) {
-        return N <= i ? std::weak_ordering::equivalent : (as[i] < bs[i] + epsilon && bs[i] < as[i] + epsilon) ?
-            compareVec(as, bs, i+1, epsilon) :
-            ((as[i] < bs[i]) ? std::weak_ordering::less : std::weak_ordering::greater);
-    }
-
-    struct Vec4Comparitor {
-        auto operator()(const vec4& lhs, const vec4& rhs) const {
-            return compareVec(lhs, rhs) == std::weak_ordering::less;
-        }
-    };
-
-    std::pair<std::vector<vec4>, std::vector<unsigned int>>
-    getVertexIndexPairs(const std::vector<vec4> &vertices) {
-        std::vector<vec4> uniqueVertices;
-        uniqueVertices.reserve(vertices.size());
-        std::vector<unsigned int> indices;
-        indices.reserve(vertices.size());
-
-        std::map<vec4, unsigned int, Vec4Comparitor> lookupMap;
-
-        for (const auto& v : vertices) {
-            const auto pos = lookupMap.find(v);
-            if (pos != lookupMap.end()) {
-                indices.push_back(pos->second);
-            } else {
-                uniqueVertices.push_back(v);
-                const auto i = static_cast<unsigned int>(lookupMap.size());
-                indices.push_back(i);
-                lookupMap.emplace(v, i);
-            }
-        }
-
-        uniqueVertices.shrink_to_fit();
-        indices.shrink_to_fit();
-
-        return std::make_pair(uniqueVertices, indices);
-    }
-
-    auto sortedEdge(auto a, auto b) {
-        return a < b ? std::make_pair(a, b) : std::make_pair(b,a);
-    }
-
-    std::set<std::pair<unsigned int, unsigned int>> getEdges(const std::vector<unsigned int> &indices) {
-        std::set<std::pair<unsigned int, unsigned int>> edges;
-        for (uint i{0}; i+2 < indices.size(); i+=3) {
-            for (const auto& edge : {
-                sortedEdge(indices.at(i), indices.at(i+1)),
-                sortedEdge(indices.at(i+1), indices.at(i+2)),
-                sortedEdge(indices.at(i+2), indices.at(i))
-            })
-                edges.insert(edge);
-        }
-
-        return edges;
     }
 }
--- src/GeometryUtils.h
+++ src/GeometryUtils.h
@@ -115,12 +115,6 @@ namespace molumes {
         return out;
     }
 
-    std::pair<std::vector<glm::vec4>, std::vector<unsigned int>> getVertexIndexPairs(const std::vector<glm::vec4>& vertices);
-    std::set<std::pair<unsigned int, unsigned int>> getEdges(const std::vector<unsigned int>& indices);
-
-    // Graham scan implementation (first point should be start point / bounds max)
-    std::vector<glm::vec2> createConvexHull(const std::vector<glm::dvec2>& points, glm::dvec2 boundingCenter = {}, const std::weak_ptr<bool>& controlFlag = {});
-
     std::optional<std::pair<std::vector<glm::vec4>, std::vector<glm::vec4>>>
     geometryPostProcessing(const std::vector<glm::vec4> &vertices, const std::weak_ptr<bool>& controlFlag,
                            float tileHeight);
